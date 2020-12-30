55aee79bc7e88ed6b0c4700fec0e791da2dfc45e
Simon Wallström <simon.wallstrom@gmail.com>
2020-12-31 00:10:14 +0100

    disabled changelog component for now

--- pages/index.js
+++ pages/index.js
@@ -50,23 +50,22 @@ export default function Home({ commit }) {
           title="Sprout – Recipe app"
           text="After hours of research I have not found any simple apps for storing your personal recipes. This is my attempt to make a good one."
         />
-        <NowPlaying />
-        <ChangeLog commit={commit} />
+        {/* <NowPlaying /> */}
+        {/* <ChangeLog commit={commit} /> */}
       </div>
     </>
   );
 }
 
-export async function getStaticProps() {
-  const query =
-    'https://api.github.com/search/issues?q=repo:simonwallstrom/simonwallstrom.com+is:pr+is:merged+author:simonwallstrom&per_page=1';
-  const res = await fetch(query);
-  const commits = await res.json();
-  const commit = commits.items[0];
-
-  return {
-    props: {
-      commit,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const query =
+//     'https://api.github.com/search/issues?q=repo:simonwallstrom/simonwallstrom.com+is:pr+is:merged+author:simonwallstrom&per_page=1';
+//   const res = await fetch(query);
+//   const commits = await res.json();
+//   const commit = commits.items[0];
+//   return {
+//     props: {
+//       commit,
+//     },
+//   };
+// }
